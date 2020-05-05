18e9fef0cca5408f0612a14f3020bed8cfe7bf96
KrugSa <43587239+KrugSa@users.noreply.github.com>
2020-05-05 11:24:37 -0500

    Consumiendo api rest con album

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'albums',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'albums',
+    loadChildren: () => import('./pages/albums/albums.module').then( m => m.AlbumsPageModule)
+  },
+  {
+    path: 'album/:id',
+    loadChildren: () => import('./pages/album/album.module').then( m => m.AlbumPageModule)
   },
 ];
 
